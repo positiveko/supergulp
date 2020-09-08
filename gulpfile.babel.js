d937ca54407f56119d75af467a586e7558d609f8
positiveko <69200669+positiveko@users.noreply.github.com>
2020-09-08 20:32:05 +0900

    make gulp environment

---
+++ gulpfile.babel.js
@@ -0,0 +1,13 @@
+import gulp from "gulp";
+import pug from "gulp-pug";
+
+const routes = {
+  pug: {
+    src: "src/*.pug",
+    dest: "build",
+  },
+};
+
+export const pug = () => gulp.src(routes.pug.src).pipe(pug()).pipe(gulp.dest());
+
+export const dev = () => console.log("I will dev");
