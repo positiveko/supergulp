3fe6890a8468d04b7688d97084a467bdec364e3e
positiveko <69200669+positiveko@users.noreply.github.com>
2020-09-09 14:57:27 +0900

    scss compiler

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,13 +1,71 @@
 import gulp from "gulp";
-import pug from "gulp-pug";
+import gpug from "gulp-pug";
+import del from "del";
+import ws from "gulp-webserver";
+import image from "gulp-image";
+import sass from "gulp-sass";
+import autoprefixer from "gulp-autoprefixer";
+import miniCSS from "gulp-csso";
+import bro from "gulp-bro";
+import babelify from "babelify";
+
+sass.compiler = require("node-sass");
 
 const routes = {
   pug: {
+    watch: "src/**/*.pug",
     src: "src/*.pug",
     dest: "build",
   },
+  img: {
+    src: "src/img/*",
+    dest: "build/img",
+  },
+  scss: {
+    watch: "src/scss/**/*.scss",
+    src: "src/scss/style.scss",
+    dest: "build/css",
+  },
+  js: {
+    watch: "src/js/**/*.js",
+    src: "src/js/main.js",
+    dest: "build/js",
+  },
 };
 
-export const pug = () => gulp.src(routes.pug.src).pipe(pug()).pipe(gulp.dest());
+const pug = () =>
+  gulp.src(routes.pug.src).pipe(gpug()).pipe(gulp.dest(routes.pug.dest));
 
-export const dev = () => console.log("I will dev");
+const clean = () => del(["build/"]);
+
+const webserver = () =>
+  gulp.src("build").pipe(ws({ livereload: true, open: true }));
+
+const img = () =>
+  gulp.src(routes.img.src).pipe(image()).pipe(gulp.dest(routes.img.dest));
+
+const styles = () =>
+  gulp
+    .src(routes.scss.src)
+    .pipe(sass().on("error", sass.logError))
+    .pipe(
+      autoprefixer({
+        browsers: ["last 2 versions"],
+      })
+    )
+    .pipe(miniCSS())
+    .pipe(gulp.dest(routes.scss.dest));
+
+const watch = () => {
+  gulp.watch(routes.pug.watch, pug);
+  gulp.watch(routes.img.src, img);
+  gulp.watch(routes.scss.watch, styles);
+};
+
+const prepare = gulp.series([clean, img]);
+
+const assets = gulp.series([pug, styles]);
+
+const postDev = gulp.parallel([webserver, watch]);
+
+export const dev = gulp.series([prepare, assets, postDev]);
